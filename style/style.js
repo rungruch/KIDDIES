ffda16a62663b4d23dcfab1c4d7f1059aa7dcd92
rungruch <43019823+rungruch@users.noreply.github.com>
2023-02-15 19:08:50 +0700

    nav-bar search changes

--- style/style.js
+++ style/style.js
@@ -30,20 +30,3 @@ function serchdisable() {
     ico_search.style.display = "block";
   
 }
- let mediaQuery = window.matchMedia('(max-width: 800px)')
- function handleTabletChange(e) {
-    if (e.matches) {
-        let search = document.getElementsByClassName("search");
-        search[0].style.display = "none";
-        let menu = document.getElementById("menu");
-        menu.style.display = "block"; 
-
-      }else{
-        let menu = document.getElementById("menu");
-        menu.style.display = "flex"; 
-        let ico_search = document.getElementById("ico-search");
-        ico_search.style.display = "block";
-      }
- }
- mediaQuery.addListener(handleTabletChange)
- handleTabletChange(mediaQuery)
